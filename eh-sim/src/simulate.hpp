a1e65caee8c87e51523b5aa9d5f93f745bb5fd9f
Mario Badr <mario.badr@outlook.com>
2017-11-16 16:49:00 -0500

    Document the simulate function

--- eh-sim/src/simulate.hpp
+++ eh-sim/src/simulate.hpp
@@ -10,6 +10,16 @@ class eh_scheme;
 struct stats_bundle;
 class voltage_trace;
 
+/**
+ * Simulate an energy harvesting device.
+ *
+ * @param binary_file The path to the application binary file.
+ * @param power The power supply over time.
+ * @param scheme The energy harvesting scheme to use.
+ * @param always_harvest true to harvest always, false to harvest during off periods only.
+ *
+ * @return The statistics tracked during the simulation.
+ */
 stats_bundle simulate(char const *binary_file,
     ehsim::voltage_trace const &power,
     eh_scheme *scheme,
