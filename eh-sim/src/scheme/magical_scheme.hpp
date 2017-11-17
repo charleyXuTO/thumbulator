b14b5803f98afa89960b409f73630187acb6a835
Mario Badr <mario.badr@outlook.com>
2017-11-17 18:53:28 -0500

    Make even more magical by improving performance

--- eh-sim/src/scheme/magical_scheme.hpp
+++ eh-sim/src/scheme/magical_scheme.hpp
@@ -44,36 +44,23 @@ public:
 
   bool will_backup(stats_bundle *stats) const override
   {
-    // always backup when it is an option
-    return true;
+    return false;
   }
 
   uint64_t backup(stats_bundle *stats) override
   {
     // do not touch arch/app state
-
-    stats->models.back().backup_times += stats->cpu.cycle_count - last_cycle_count;
-    stats->models.back().num_backups++;
-
-    last_cycle_count = stats->cpu.cycle_count;
-
     return 0;
   }
 
   uint64_t restore(stats_bundle *stats) override
   {
     // do not touch arch/app state
-
-    // allocate space for a new active period model
-    stats->models.emplace_back();
-
     return 0;
   }
 
 private:
   capacitor battery;
-
-  uint64_t last_cycle_count = 0u;
 
   // based on Mementos numbers for an MSP430
   static constexpr auto energy_per_instruction = MEMENTOS_INSTRUCTION_ENERGY + MEMENTOS_REG_FLASH;
