19615b6a6ac3c4d14da30f6d5a35930f77ed4676
Karthik Ganesan <karthik.ganesan@mail.utoronto.ca>
2018-07-23 17:00:46 -0400

    modified gitignore

---
+++ thumbulator/CMakeFiles/thumbulator.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/karthik/school/clank/eh-sim/thumbulator/thumbulator/src/cpu.cpp" "/home/karthik/school/clank/eh-sim/thumbulator/thumbulator/CMakeFiles/thumbulator.dir/src/cpu.cpp.o"
+  "/home/karthik/school/clank/eh-sim/thumbulator/thumbulator/src/decode.cpp" "/home/karthik/school/clank/eh-sim/thumbulator/thumbulator/CMakeFiles/thumbulator.dir/src/decode.cpp.o"
+  "/home/karthik/school/clank/eh-sim/thumbulator/thumbulator/src/exmemwb_arith.cpp" "/home/karthik/school/clank/eh-sim/thumbulator/thumbulator/CMakeFiles/thumbulator.dir/src/exmemwb_arith.cpp.o"
+  "/home/karthik/school/clank/eh-sim/thumbulator/thumbulator/src/exmemwb_branch.cpp" "/home/karthik/school/clank/eh-sim/thumbulator/thumbulator/CMakeFiles/thumbulator.dir/src/exmemwb_branch.cpp.o"
+  "/home/karthik/school/clank/eh-sim/thumbulator/thumbulator/src/exmemwb_logic.cpp" "/home/karthik/school/clank/eh-sim/thumbulator/thumbulator/CMakeFiles/thumbulator.dir/src/exmemwb_logic.cpp.o"
+  "/home/karthik/school/clank/eh-sim/thumbulator/thumbulator/src/exmemwb_mem.cpp" "/home/karthik/school/clank/eh-sim/thumbulator/thumbulator/CMakeFiles/thumbulator.dir/src/exmemwb_mem.cpp.o"
+  "/home/karthik/school/clank/eh-sim/thumbulator/thumbulator/src/exmemwb_misc.cpp" "/home/karthik/school/clank/eh-sim/thumbulator/thumbulator/CMakeFiles/thumbulator.dir/src/exmemwb_misc.cpp.o"
+  "/home/karthik/school/clank/eh-sim/thumbulator/thumbulator/src/memory.cpp" "/home/karthik/school/clank/eh-sim/thumbulator/thumbulator/CMakeFiles/thumbulator.dir/src/memory.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "thumbulator/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ thumbulator/CMakeFiles/thumbulator.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/thumbulator.dir/src/decode.cpp.o"
+  "CMakeFiles/thumbulator.dir/src/cpu.cpp.o"
+  "CMakeFiles/thumbulator.dir/src/exmemwb_arith.cpp.o"
+  "CMakeFiles/thumbulator.dir/src/exmemwb_branch.cpp.o"
+  "CMakeFiles/thumbulator.dir/src/exmemwb_logic.cpp.o"
+  "CMakeFiles/thumbulator.dir/src/exmemwb_mem.cpp.o"
+  "CMakeFiles/thumbulator.dir/src/exmemwb_misc.cpp.o"
+  "CMakeFiles/thumbulator.dir/src/memory.cpp.o"
+  "libthumbulator.pdb"
+  "libthumbulator.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/thumbulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
